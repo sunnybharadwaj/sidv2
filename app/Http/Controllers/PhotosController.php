c13cb184658d9cb1f5c6d0cad13291460ff3fc2f
Sunny Bharadwaj <sunny@chakradesign.co>
2019-06-14 12:48:24 +0530

    Permission fixes

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -49,10 +49,10 @@ class PhotosController extends Controller
 
         //        Store Images in directory
         $hd_file = $request->file('photo_hd');
-        $hd_url = $hd_file->store('photos_hd', ['disk' => 'images']);
+        $hd_url = $hd_file->putFile('photos_hd', ['disk' => 'images']);
 
         $sd_file = $request->file('photo_sd');
-        $sd_url = $sd_file->store('photos_sd', ['disk' => 'images']);
+        $sd_url = $sd_file->putFile('photos_sd', ['disk' => 'images']);
 
 
 //        Create a storage stack
